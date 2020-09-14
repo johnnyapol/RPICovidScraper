50ba9f0041f1117bb3775b38dc25063b50db1920
John C. Allwein <git@johnnyapol.me>
2020-09-14 12:23:39 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+RPI Covid Dashboard Scrapper
+----------------------------
+
+Scrapes https://covid19.rpi.edu/dashboard and (optionally) posts to a discord channel using webhooks.
+
+Usage: python3 main.py <DISCORD_WEBHOOK_URL> or ./main.py <DISCORD_WEBHOOK_URL>
+
+See requirements.txt for dependencies
---
+++ main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# Usage: ./main.py <DISCORD_WEBHOOK_URL>
+'''
+Copyright (C) 2020 John C. Allwein 'johnnyapol' (admin@johnnyapol.me)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+import requests
+import discord_webhook
+from discord_webhook import DiscordEmbed
+from bs4 import BeautifulSoup
+from random import choice
+import sys
+
+
+def check_for_updates():
+    request = requests.get("https://covid19.rpi.edu/dashboard")
+    soup = BeautifulSoup(request.text, features="lxml")
+    header = "field field--name-field-stats field--type-entity-reference-revisions field--label-hidden field__items"
+    header2 = "field field--name-field-stat field--type-string field--label-hidden field__item"
+    data = soup.find("div", {"class": header})
+    data = data.findAll("div", {"class": header2})
+
+    """
+        Current data format:
+
+        case_data[0] = positive tests (last 24 hours)
+        case_data[1] = positive test results (last 7 days)
+        case_data[2] = positive test results (since august 17th)
+        case_data[3] = total tests (last 7 days)
+        case_data[4] = total tests (since august 17th)
+    """
+    case_data = [x.text for x in data]
+
+    return case_data
+
+
+def post_discord(case_data):
+    thumbnails = [
+        "https://www.continentalmessage.com/wp-content/uploads/2015/09/123rf-alert2.jpg",
+        "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans/5671259/7923c9b8e0a5799d4d422208b31f5ca0f4f49067.png",
+    ]
+
+    # Calculate weekly positivity rate
+    # Need to strip commas out
+    pcr = (
+        int(case_data[1].replace(",", "")) / int(case_data[3].replace(",", ""))
+    ) * 100
+
+    embed = DiscordEmbed(description="RPI Covid Dashboard Updated", color=242424)
+    embed.set_thumbnail(url=choice(thumbnails))
+    embed.add_embed_field(name="Positive Tests (last 24 hours)", value=case_data[0])
+    embed.add_embed_field(
+        name="Positive Test Results (last 7 days)", value=case_data[1]
+    )
+    embed.add_embed_field(name="Total Tests (last 7 days)", value=case_data[3])
+    embed.add_embed_field(name="Weekly Positivty Rate", value=f"{round(pcr, 4)}%")
+    embed.add_embed_field(
+        name="Positive Test Results (since August 17th)", value=case_data[2]
+    )
+    embed.add_embed_field(name="Total Tests (since August 17th)", value=case_data[4])
+    embed.set_author(
+        name="Click for RPI COVID Dashboard", url="https://covid19.rpi.edu/dashboard"
+    )
+
+    hook = discord_webhook.DiscordWebhook(url=sys.argv[1])
+    hook.add_embed(embed)
+    hook.execute()
+
+
+def load_previous():
+    case_data = []
+    try:
+        with open(".cache", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                case_data.append(line.rstrip())
+    except:
+        print("Cache read failed")
+    return case_data
+
+
+def save(case_data):
+    with open(".cache", "w") as file:
+        for case in case_data:
+            file.write(case + "\n")
+
+
+def main():
+    previous_case_data = load_previous()
+    current_case_data = check_for_updates()
+
+    if current_case_data != previous_case_data:
+        if len(sys.argv) < 2:
+            print("Skipping posting to discord as argv[1] a/k/a webhook URL is missing")
+        else:
+            post_discord(current_case_data)
+        save(current_case_data)
+    print(f"Done. Old: {previous_case_data} New: {current_case_data}")
+
+
+if __name__ == "__main__":
+    main()
