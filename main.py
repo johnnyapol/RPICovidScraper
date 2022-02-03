johnnyapol/RPICovidScraper

commit 809d543dd226e5623b9d84ae8bcb01e323beb04b

covid_data: cleanup update() to work with multiple entries in a single day

main.py
@@ -46,19 +46,15 @@ DASHBOARD = "https://covid19.rpi.edu/dashboard"
 class CovidData:
     def __init__(self):
         self.rpi_array = [0] * 5
-        self.last_updated = date.today() - timedelta(days=1)
         self.historicalData = {}
 
     def update(self, case_data):
         today = date.today()
-
-        if today != self.last_updated:
-            self.last_updated = today
-            self.historicalData[today] = case_data
+        self.historicalData[today] = case_data
         self.rpi_array = case_data
 
     def get_rolling(self):
-        return sum(self.get_rolling_iterator(self.last_updated))
+        return sum(self.get_rolling_iterator())
 
     def get_case_data(self):
         return self.rpi_array
@@ -95,7 +91,7 @@ def check_for_updates():
     """
     return (
         [
-            # Cleanup text (remove commas, all whitespace) so python can parse it
+            # Cleanup text (remove commas, all whitespace) so python can parse ita
             int("".join(x.text.replace(",", "").split()))
             for x in soup.find("div", {"class": header}).findAll(
                 "div", {"class": header2}

commit bf66bdcb3c0f6c447f4768cec94e7c2e9696bdd9

covid_data: fix regression in 14 day deltas
Because of a regression introduced in 809d543dd226e5623b9d84ae8bcb01e323beb04b,
the delta in the 14 day rolling sum was just ignoring
the days that dropped off since the last update, so
it would only go up.

To fix this, we need to compute old_rolling from the last day
we had data.

main.py
@@ -66,6 +66,11 @@ class CovidData:
             for date in dates
         ]
 
+    def get_last_update(self):
+        return (
+            None if len(self.historicalData) == 0 else max(self.historicalData.keys())
+        )
+
 
 def check_for_updates():
     global DASHBOARD
@@ -323,7 +328,11 @@ def main():
             print(f"Page archived failed")
             traceback.print_exc()
 
-        old_rolling = covid_data.get_rolling()
+        last_update = covid_data.get_last_update()
+        if last_update is None:
+            old_rolling = 0
+        else:
+            old_rolling = sum(covid_data.get_rolling_iterator(day=last_update))
         covid_data.update(current_case_data)
 
         post_discord(